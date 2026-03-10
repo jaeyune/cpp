dd3f4d54494d0d1bd2b1c7baf897bd6c70aa2358
jaeyune <lee021104@naver.com>
2026-03-10 13:55:59 +0900

    .

---
+++ Week_2/P53_2.1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+  int main()
+{
+    printf(" _       _____   __   __   \n");
+    printf("| |        | |    \\ \\ / /\n");
+    printf("| |        | |     \\ V /  \n");
+    printf("| |___     | |      | |    \n");
+    printf("|_____|    |_|      |_|    \n");
+}  
